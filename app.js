742ed8543be91fb4f5b120e179fd0c5e02a08224
Daniel Stephany <danielstephany@daniels-mbp.lan>
2023-11-04 19:53:38 -0700

    added express

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const http = require("http")
-const handleRoutes = require("./routes")
+const express = require("express")
 
-const server = http.createServer(handleRoutes)
+const port = 3001
+const app = express();
 
-console.log("listening on port: 3000")
-server.listen(3000)
+app.use("/", (req, res, next) => {
+    console.log("always called")
+    next()
+})
+
+app.use("/add-product", (req, res, next) => {
+    console.log("middle ware 1")
+    res.send(`<h1>The "add product" page</h1>`)
+})
+
+app.use("/", (req, res, next) => {
+    console.log("home")
+    res.send("<h1>Home</h1>")
+})
+
+app.listen(port, () => {
+    console.log("listening on port: 3000")
+})
